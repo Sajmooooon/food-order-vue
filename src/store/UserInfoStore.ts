7cca723bf3958cc8e3082613fb91cb3a81fd56a2
simon <sajmoooonsk@gmail.com>
2023-08-08 16:26:19 +0200

    feat: add user object

--- src/store/UserInfoStore.ts
+++ src/store/UserInfoStore.ts
@@ -1,8 +1,16 @@
 import {defineStore} from "pinia";
+import User from "@/types/User";
 
 export const useUserInfoStore = defineStore('userInfoStore',{
     state: ()=>({
-        loginUser: true
+        loginUser: true,
+        user: {
+            name: 'Jozko',
+            lastName: 'Mrkvicka',
+            city: 'Bratislava',
+            street: 'Bernolakova',
+            psc: 556565,
+            phone: '+4218984565645'} as User
     }),
 
     actions: {
@@ -10,6 +18,11 @@ export const useUserInfoStore = defineStore('userInfoStore',{
             this.loginUser = false
             console.log(this.loginUser)
 
+        },
+
+        updateUser(newUser: User ){
+            this.user = newUser
+            console.log(this.user)
         }
     }
 
---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+interface User {
+    name: string,
+    lastName: string,
+    city: string,
+    street: string,
+    psc: number,
+    phone: string,
+}
+
+export default User
