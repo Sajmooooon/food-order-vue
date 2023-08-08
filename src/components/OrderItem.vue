1e8166a2da02d73acc695915486e8018153bb664
simon <sajmoooonsk@gmail.com>
2023-08-08 21:23:30 +0200

    feat: add order item

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class=" px-5 py-2.5">
+    <div class="border-b-2 border-gray-300 justify-between flex py-5 items-center gap-2">
+
+      <div class="flex items-center gap-2">
+        <i class='bx bx-trash text-violet-700 text-3xl'></i>
+
+        <div class="flex-col text-left">
+          <div class="font-bold">Salami</div>
+          <div class=""> 255€</div>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <div class=" flex justify-center flex items-center ">
+          <i class='bx bx-plus border rounded-l  border-2 py-2 px-1 bg-white border-indigo-700 text-base cursor-pointer' ></i>
+          <input type="number" v-model="test" class=" w-[35px] p-2 text-center  border-indigo-700 border-y-2 text-base  w-full h-auto outline-none" />
+          <i class='bx bx-minus rounded-r  border-y-2 py-2 px-1 border-2 border-indigo-700 text-base bg-white cursor-pointer' ></i>
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderItem",
+  data(){
+    return{
+      test: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+input[type="number"] {
+  -webkit-appearance: textfield;
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+</style>
