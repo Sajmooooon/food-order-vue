5ec55b1425533c2231ca151ae8b20fa51d1a1706
simon <sajmoooonsk@gmail.com>
2023-08-08 16:27:20 +0200

    feat: add empty order list component

---
+++ src/components/OrderList.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="h-full overflow-y-auto flex flex-col mb-2">
+  <div class="p-2.5 text-xl" v-for="i in arr" :key="i">{{i}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "OrderList",
+  data(){
+    return{
+      arr: [1,2,3,4,5]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
