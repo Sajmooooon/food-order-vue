91d6495c0d0123438a594c3d1965d39061b77272
simon <sajmoooonsk@gmail.com>
2023-08-09 19:37:18 +0200

    feat: add select filter for mobile devices

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="w-full bg-white rounded-lg mb-10 flex items-start shadow-[5px_5px_10px_rgba(0,0,0,0.2)]">
-    <div class="p-10 w-full left-0 right-auto flex justify-between items-center gap-5">
+  <div class="w-full bg-white rounded-lg mb-10 flex flex-col items-start shadow-[5px_5px_10px_rgba(0,0,0,0.2)]">
+    <div class="search-items">
+
+
+    <div class=" w-full left-0 right-auto flex justify-between items-center gap-5">
       <div class="text-left">
         <h4 class="text-3xl font-bold">Pizzeria</h4>
         <p class="italic">Quick and Tasty </p>
@@ -12,13 +15,25 @@
         </div>
 
         <div class="background-violet rounded-2xl cursor-pointer select-none" @click="cartItemStore.toggleCart()">
-          <div class="py-3 px-5 text-2xl">
+          <div class="py-3 px-5 flex items-center text-2xl">
             <i class='bx bx-cart'></i>
             <span v-if="numberOfItems>0">{{numberOfItems}}</span>
           </div>
         </div>
 
       </div>
+
+
+    </div>
+    <div class="flex m-item p-0 mt-5 text-left w-full items-center text-2xl">
+      <span class="font-bold mr-2">Filter:</span>
+      <select v-model="selectedItem" @change="foodItemStore.changeSelectedItem(selectedItem)" class="outline-none text-center text-3xl outline-none border-b-2 border-l-0 background-violet p-2 rounded-2xl">
+        <option value="Pizza">Pizza</option>
+        <option value="Dish">Dish</option>
+        <option value="Soup">Soup</option>
+        <option value="Salad">Salad</option>
+      </select>
+    </div>
     </div>
   </div>
 </template>
@@ -29,6 +44,7 @@ import {useFoodItemsStore} from "@/store/FoodItemsStore";
 import {useCartItemsStore} from "@/store/CartItemsStore";
 import {useOrderItemsStore} from "@/store/OrderItemsStore";
 import {storeToRefs} from "pinia/dist/pinia";
+import SelectItem from "@/types/SelectItem";
 
 export default {
   name: "SearchBar",
@@ -38,6 +54,10 @@ export default {
     const cartItemStore= useCartItemsStore()
     const orderItemStore = useOrderItemsStore()
 
+    const {selectedItem} = storeToRefs(foodItemStore)
+    // return {currentItem,store}
+
+
     const {numberOfItems} = storeToRefs(orderItemStore)
 
 
@@ -46,7 +66,7 @@ export default {
       search.value = ''
     }
 
-    return {search,searchItem, cartItemStore, numberOfItems}
+    return {search,searchItem, cartItemStore, numberOfItems,selectedItem,foodItemStore}
   }
 }
 </script>
